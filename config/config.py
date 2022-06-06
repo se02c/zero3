cca38832813635db87d1a45555539695aabe4918
VAmBIRR <106986037+VAmBIRR@users.noreply.github.com>
2022-06-06 17:47:34 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VamBIR/end",
+    "https://github.com/VamBIRR/end",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
