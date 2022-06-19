f24e02d50b7c375d401f41ba37fb39822b235e32
VAmBIRR <106986037+VAmBIRR@users.noreply.github.com>
2022-06-19 02:49:57 +0200

    Update Id.py

--- YukkiMusic/plugins/play/Id.py
+++ YukkiMusic/plugins/play/Id.py
@@ -9,7 +9,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     command(["ايدي"])
     & ~filters.edited
 )
-async ah vambir(client: Client, message: Message):
+async def vambir(client: Client, message: Message):
     usr = await client.get_users(message.from_user.id)
     name = usr.first_name
     async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
