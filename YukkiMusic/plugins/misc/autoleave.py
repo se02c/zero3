595431a1781abf963d9b09db64666e74feb9ce60
VAMBIR <96371146+VamBIR@users.noreply.github.com>
2022-06-29 01:26:26 +0200

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -75,7 +75,7 @@ async def auto_end():
                 try:
                     await app.send_message(
                         chat_id,
-                        "Bot has left voice chat due to inactivity to avoid overload on servers. No-one was listening to the bot on voice chat.",
+                        "#VAMBIR\n◍ خرج المساعد من المحادثه الصوتيه\nوذلك لعدم وجود شخص في المحادثه\nوهذا اهدار لسيرفر √",
                     )
                 except:
                     continue
