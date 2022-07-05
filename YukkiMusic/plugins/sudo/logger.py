9958ce62872ca9787fb9c355622502f97903bdd0
VAMBIR <96371146+VamBIR@users.noreply.github.com>
2022-07-05 03:33:46 +0200

    Update logger.py

--- YukkiMusic/plugins/sudo/logger.py
+++ YukkiMusic/plugins/sudo/logger.py
@@ -13,7 +13,7 @@ from pyrogram import filters
 import config
 from strings import get_command
 from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+from config.config import OWNER_ID
 from YukkiMusic.utils.database import add_off, add_on
 from YukkiMusic.utils.decorators.language import language
 
@@ -21,7 +21,7 @@ from YukkiMusic.utils.decorators.language import language
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
 
 
-@app.on_message(command(LOGGER_COMMAND) & SUDOERS)
+@app.on_message(command(LOGGER_COMMAND) & filters.user(OWNER_ID))
 @language
 async def logger(client, message, _):
     usage = _["log_1"]
