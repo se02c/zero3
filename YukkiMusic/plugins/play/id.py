5015b3906b0324b473d3f9ae47d72bd7f698157a
VAMBIR <96371146+VamBIR@users.noreply.github.com>
2022-07-08 13:39:56 +0200

    Add files via upload

---
+++ YukkiMusic/plugins/play/id.py
@@ -0,0 +1,29 @@
+import asyncio
+from YukkiMusic import app
+from strings.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+@app.on_message(
+    command(["ايدي"])
+    & filters.group
+    & ~filters.edited
+)
+async def vambir(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""◂ اسمك ↫ {message.from_user.mention}\n\n◂ معرفك ↫ @{message.from_user.username}\n\n◂ ايديك ↫ `{message.from_user.id}`\n\n◂ ايدي الجروب ↫ `{message.chat.id}`""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                       "🔮𝐒𝐎𝐔𝐑𝐂𝐄 𝐕𝘼𝙈𝘽𝙄𝙍🔮", url=f"https://t.me/XxvprxX"),
+                ],
+                [  
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        ),
+    )
